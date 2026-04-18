223ac1a79811b16186517aafc69e982f4c7858e5
Thasleema <thasleemapk133@gmail.com>
2026-04-18 21:04:31 +0530

    feat: finalize user model with schema and timestamps

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import  express from "express"
 import dotenv from "dotenv"
+import connectDB from "./config/dbconfig.js";
+
 dotenv.config()
 const app =express()
 app.use(express.json())
 app.use('/')
+connectDB()
+
 
 const PORT = process.env.PORT || 5000;
 
