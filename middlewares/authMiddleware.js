b884872ce51131e611f23399fea92241d6b22d9a
Thasleema <thasleemapk133@gmail.com>
2026-04-18 23:51:27 +0530

    feat: add auth middleware for protected routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken"
+export const authMiddleware=(req,res,next)=>{
+const authHeader=req.headers.authorization
+if(!authHeader){
+    return res.status(401).json({message:"Unauthorized User"})
+}
+const token=authHeader.split('')[1]
+jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+       if(err){
+            return res.status(403).json({message:"forbidden"})
+
+    }
+    console.log("user from toen",user);
+    
+    req.user=user
+    next()
+})
+}
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,6 @@
 import { loginUser, registerUser } from "../controllers/authentication";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 
 userRouter.post("/register", registerUser);
-userRouter.get("/login",loginUser)
+userRouter.get("/login",authMiddleware, loginUser)
