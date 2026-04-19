548217f8986ab34e063d54195cd38b1190a8ac13
Thasleema <thasleemapk133@gmail.com>
2026-04-19 12:08:50 +0530

    feat:implement update the product

--- controllers/generation/productController.js
+++ controllers/generation/productController.js
@@ -1,35 +1,69 @@
-const createProduct=async (req,res) => {
+const createProduct = async (req, res) => {
     try {
-        const {name,price,category,description}=req.body
-    if(!name|| !price|| !category||!description){
-return res.status(400).json({meassage:"All fields required"})
-    }
-    const product=await productModel.create({
-        name,
-        price,
-        category,
-        description
-    })
-    res.status(201).json({message:"Product created ",product})
+        const { name, price, category, description } = req.body
+        if (!name || !price || !category || !description) {
+            return res.status(400).json({ meassage: "All fields required" })
+        }
+        const product = await productModel.create({
+            name,
+            price,
+            category,
+            description
+        })
+        res.status(201).json({ message: "Product created ", product })
 
-        
+
     } catch (error) {
-        res.status(500).json({meassage:"server error"})
+        res.status(500).json({ message: "server error" })
     }
 }
 
 
 
-const getAllProduct=async (req,res) => {
+const getAllProduct = async (req, res) => {
     try {
-        const product=await productModel.find()
-        return res.status(200).json({message:"Product find",product:product})
+        const product = await productModel.find()
+        return res.status(200).json({ message: "Product find", product: product })
     } catch (error) {
-        res.status(500).json({message:"server error"})
+        res.status(500).json({ message: "server error" })
     }
 }
+const getSingleProduct = async (req, res) => {
+    try {
+        const product = await productModel.findById(req.params.productId)
+        if (!product) {
+          return  res.status(404).json({ message: "Product Not Found" })
+        }
+         res.status(200).json({ message: "Product Found", product: product })
 
-export{
+    } catch (error) {
+        res.status(500).json({ message: "server error" })
+
+    }
+
+}
+
+const updateProduct=async (req,res) => {
+    try {
+const product=productModel.findByIdAndUpdate(req.params.productId
+    ,{$set:req.body},
+    {new:true}
+)
+    
+if(!product){
+    return res.status(404).json({message:"Product  not Found"})
+}
+res.status(200).json({message:"product updated",Product})
+    } catch (error) {
+                res.status(500).json({ message: "server error" })
+
+    }
+    
+}
+
+export {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    getSingleProduct,
+    updateProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import authMiddleware from "../middlewares/authMiddleware.js"
 import roleMiddleware  from "../middlewares/roleMiddleware.js"
-import { createProduct, getAllProduct } from "../controllers/generation/productController.js"
+import { createProduct, getAllProduct, getSingleProduct } from "../controllers/generation/productController.js"
 const productrouter=express.Router()
 
 
 productrouter.post("/create",authMiddleware,roleMiddleware("admin"),createProduct)
-productrouter.get("/",getAllProduct)
+productrouter.get("/all",authMiddleware,getAllProduct)
+productrouter.get("/:productId",authMiddleware,getSingleProduct)
+productrouter.put("/:productId", authMiddleware, roleMiddleware("admin"), updateProduct)
 export default productrouter
