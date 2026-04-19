f7f3538f93273ba12b7316de5f6ec2d73ce5860f
Thasleema <thasleemapk133@gmail.com>
2026-04-19 12:52:57 +0530

    feat:implement delete product feature

--- controllers/generation/productController.js
+++ controllers/generation/productController.js
@@ -2,7 +2,7 @@ const createProduct = async (req, res) => {
     try {
         const { name, price, category, description } = req.body
         if (!name || !price || !category || !description) {
-            return res.status(400).json({ meassage: "All fields required" })
+            return res.status(400).json({ message: "All fields required" })
         }
         const product = await productModel.create({
             name,
@@ -10,7 +10,7 @@ const createProduct = async (req, res) => {
             category,
             description
         })
-        res.status(201).json({ message: "Product created ", product })
+        return res.status(201).json({ message: "Product created ", product })
 
 
     } catch (error) {
@@ -45,7 +45,7 @@ const getSingleProduct = async (req, res) => {
 
 const updateProduct=async (req,res) => {
     try {
-const product=productModel.findByIdAndUpdate(req.params.productId
+const product=await productModel.findByIdAndUpdate(req.params.productId
     ,{$set:req.body},
     {new:true}
 )
@@ -53,9 +53,22 @@ const product=productModel.findByIdAndUpdate(req.params.productId
 if(!product){
     return res.status(404).json({message:"Product  not Found"})
 }
-res.status(200).json({message:"product updated",Product})
+res.status(200).json({message:"product updated",product})
     } catch (error) {
                 res.status(500).json({ message: "server error" })
+
+    }
+    
+}
+const deleteProduct=async (req,res) => {
+    try {
+        const product=await productModel.findByIdAndDelete(req.params.productId)
+        if (!product){
+            return res.status(404).json({message:"Product not found"})
+        }
+        res.status(200).json({message:"Deleted sucessfully",product})
+    } catch (error) {
+                        res.status(500).json({ message: "server error" })
 
     }
     
@@ -65,5 +78,6 @@ export {
     createProduct,
     getAllProduct,
     getSingleProduct,
-    updateProduct
+    updateProduct,
+    deleteProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from "express"
 import authMiddleware from "../middlewares/authMiddleware.js"
 import roleMiddleware  from "../middlewares/roleMiddleware.js"
-import { createProduct, getAllProduct, getSingleProduct } from "../controllers/generation/productController.js"
+import { createProduct,  deleteProduct, getAllProduct, getSingleProduct, updateProduct } from "../controllers/generation/productController.js"
 const productrouter=express.Router()
 
 
 productrouter.post("/create",authMiddleware,roleMiddleware("admin"),createProduct)
 productrouter.get("/all",authMiddleware,getAllProduct)
 productrouter.get("/:productId",authMiddleware,getSingleProduct)
-productrouter.put("/:productId", authMiddleware, roleMiddleware("admin"), updateProduct)
+productrouter.put("/:productId", authMiddleware, roleMiddleware("admin"),updateProduct)
+productrouter.delete("/:productId",authMiddleware,roleMiddleware("admin"),deleteProduct)
 export default productrouter
