1d034c6a87e2280adf8339d96aa693a2703880f9
Thasleema <thasleemapk133@gmail.com>
2026-04-19 11:45:26 +0530

    feat:implement getallproduct

--- controllers/generation/productController.js
+++ controllers/generation/productController.js
@@ -17,6 +17,19 @@ return res.status(400).json({meassage:"All fields required"})
         res.status(500).json({meassage:"server error"})
     }
 }
+
+
+
+const getAllProduct=async (req,res) => {
+    try {
+        const product=await productModel.find()
+        return res.status(200).json({message:"Product find",product:product})
+    } catch (error) {
+        res.status(500).json({message:"server error"})
+    }
+}
+
 export{
-    createProduct
+    createProduct,
+    getAllProduct
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
 import authMiddleware from "../middlewares/authMiddleware.js"
 import roleMiddleware  from "../middlewares/roleMiddleware.js"
-import { createProduct } from "../controllers/generation/productController.js"
+import { createProduct, getAllProduct } from "../controllers/generation/productController.js"
 const productrouter=express.Router()
-productrouter.post("/",authMiddleware,roleMiddleware("admin"),createProduct)
 
+
+productrouter.post("/create",authMiddleware,roleMiddleware("admin"),createProduct)
+productrouter.get("/",getAllProduct)
 export default productrouter
