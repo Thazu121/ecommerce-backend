b4d614501bdd43b0d74d6611876d9aa8bb64bade
Thasleema <thasleemapk133@gmail.com>
2026-05-08 14:08:45 +0530

    added the vercel url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-frontend-theta-bay.vercel.app/",
     credentials: true,
   })
 );
