6aeac266b7c4cf79e50e71da56f4f02dfd1cfc3d
Thasleema <thasleemapk133@gmail.com>
2026-05-08 13:44:12 +0530

    update index.js for contact

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-import  express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
 import connectDB from "./config/dbConnection.js";
-import userRouter from "./routes/userRouter.js"
+
+import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRoutes.js";
-import authRouter from "./routes/authenticationRoute.js"
+import authRouter from "./routes/authenticationRoute.js";
 import orderRouter from "./routes/orderRoutes.js";
+import adminrouter from "./routes/adminuserRouter.js";
+import contactRouter from "./routes/contactRoutes.js";
 import errorHandler from "./middlewares/errorHandler.js";
-import analyticRouter from "./routes/analyticsRoutes.js";
 
-dotenv.config()
-const app =express()
-app.use(express.json())
-app.use('/auth',authRouter)
-app.use('/user',userRouter)
-app.use("/products",productRouter)
-app.use("/order",orderRouter)
-app.use("/analytics", analyticRouter);
+dotenv.config();
 
-app.use(errorHandler)
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 
+app.use("/uploads", express.static("uploads"));
+
+
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/products", productRouter);
+app.use("/order", orderRouter);
+app.use("/admin", adminrouter);
+app.use("/contact", contactRouter);
+
+app.use(errorHandler);
+
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
-connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
